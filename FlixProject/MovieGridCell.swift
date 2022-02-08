e59c655800c3634669b5d5dd46f65c2a85484e61
sid-pandya <86027069+sid-pandya@users.noreply.github.com>
2022-02-07 23:19:01 -0800

    Unit-2

---
+++ FlixProject/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCellCollectionViewCell.swift
+//  FlixProject
+//
+//  Created by Sidhdharth Pandya on 2/7/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
